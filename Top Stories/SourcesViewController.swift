5981aa1d6e84fcde6d466d835d30cc848ea37011
Alush Benitez <alushb5@yahoo.com>
2018-07-10 10:38:23 -0500

    Finished initial set up of SourcesTableView

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -28,7 +28,7 @@ class SourcesViewController: UITableViewController {
                 }
             }
         }
-        loadError() 
+        loadError()
     }
     
     func parse(json:JSON){
@@ -47,6 +47,19 @@ class SourcesViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
